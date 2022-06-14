3f1c9a1c7f03b5248ad33c1396f3e4ab49b5c810
Shin-jun <inside1451@naver.com>
2022-06-14 17:55:24 +0900

    Add CreditView
    크레딧뷰 추가했습니다.

---
+++ MC2-Team1/Views/CreditView.swift
@@ -0,0 +1,20 @@
+//
+//  CreditView.swift
+//  MC2-Team1
+//
+//  Created by Shin yongjun on 2022/06/14.
+//
+
+import SwiftUI
+
+struct CreditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreditView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreditView()
+    }
+}
