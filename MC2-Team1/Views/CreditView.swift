0eef26bdffa786ec94fe2e49c8f90c68a3148a50
Shin-jun <inside1451@naver.com>
2022-06-15 02:16:34 +0900

    [Update] EndCreditView & [ADD] BackgroundImage
    엔딩크레딧뷰 회전 시간 수정, 다크 모드 백그라운드 이미지 추가

--- MC2-Team1/Views/CreditView.swift
+++ MC2-Team1/Views/CreditView.swift
@@ -12,14 +12,19 @@ struct CreditView: View {
     @State private var animationEnd = false
     // animation 지속시간
     private let startAnimationDuration = 5.0
+    private let middleAnimationDuration = 7.0
     private let endAnimationDuration = 0.5
+    private let mainFont = "NanumMyeongjo"
+    @Environment(\.colorScheme) var colorScheme: ColorScheme
     @State var opacity: Double = 0
     
     var body: some View {
         ZStack {
-            Text("Hello Woody! \nHello Digi! \nHello Bethev! \nHello EllyJ! \nHello Rang! \nHello Sophie! \nHello Everett \n\n\n\n\n\n\n")
+            Image("EndCreditBackgroud-1")
+                .resizable()
+            Text("Hello Woody! \nHello Digi! \nHello Bethev! \nHello EllyJ! \nHello Rang! \nHello Sophie! \nHello Everett! \n\n\n\n Thank you")
                 .fontWeight(.bold)
-                .font(.system(size: 40, design: .rounded))
+                .font(.custom(mainFont, size: 35))
                 .multilineTextAlignment(.center)
                 .lineLimit(nil)
                 .truncationMode(.head)
@@ -27,7 +32,7 @@ struct CreditView: View {
                 .padding()
             
             
-                .rotation3DEffect(.degrees(animationEnd ? 0 : 50), axis: (x: 1, y: 0, z: 0))
+                .rotation3DEffect(.degrees(animationEnd ? 0 : 35), axis: (x: 1, y: 0, z: 0))
                 .shadow(color: .gray, radius: 3, x: 0, y: 10)
                 .frame(width: 400, height: animationStart ? 750 : 0)
                 //.animation(Animation.linear(duration: startAnimationDuration), value: opacity)
@@ -36,12 +41,13 @@ struct CreditView: View {
                     self.animationStart.toggle()
                     opacity = 1
                     // animation 시간 후 animationEnd true
-                    DispatchQueue.main.asyncAfter(deadline: .now() + self.startAnimationDuration) {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + self.middleAnimationDuration) {
                             self.animationEnd.toggle()
                         
                     }
             }
         }
+        .ignoresSafeArea()
         
     }
 }
